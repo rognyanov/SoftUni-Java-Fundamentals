b973d9a2f5fb2609a525cad06871213d6a028c67
rognyanov <radoslav.ognyanov@outlook.com>
2019-07-29 17:06:34 +0300

    Started exercise 6.

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/Foreign Languages/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HashMap<String, String> languages = new HashMap<String, String>(){{
+			put("USA", "English");
+			put("England", "English");
+			put("Spain", "Spanish");
+			put("Argentina", "Spanish");
+			put("Mexico", "Spanish");
+		}};
+
+	}
+
+}
