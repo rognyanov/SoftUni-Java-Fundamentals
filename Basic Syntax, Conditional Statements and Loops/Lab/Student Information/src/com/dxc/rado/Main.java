3aec3e18b4d773e92460cf6211ff7f86d4d3ac1a
Rado <Rado@10.0.2.15>
2019-07-29 12:24:06 +0300

    Lab / 1.Student Information

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/Student Information/src/com/dxc/rado/Main.java
@@ -0,0 +1,18 @@
+package com.dxc.rado;
+
+import java.util.Scanner;
+
+public final class Main {
+
+	public static void main(String[] args) {		
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		Integer age = scanner.nextInt();
+		Double grade = scanner.nextDouble();
+		
+		scanner.close();
+		
+		System.out.println(String.format("Name: %s, Age: %d, Grade: %.2f", name, age, grade));
+	}
+
+}
