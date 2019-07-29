eaa12f898f2cdf8e48d16cd26d05a3ffb80933c8
rognyanov <radoslav.ognyanov@outlook.com>
2019-07-29 12:58:15 +0300

    Lab / 2.Passed

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/Passed/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public final class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Double grade = scanner.nextDouble();
+		
+		if(grade>=3) {
+			System.out.println("Passed!");
+		}
+	}
+
+}
