cfe455d5c9ded2f3b2d80b629acbf34625dfa292
rognyanov <radoslav.ognyanov@outlook.com>
2019-07-31 11:27:09 +0300

    Lab / 7.Reversed Chars

---
+++ Data Types and Variables/Lab/Reversed Chars/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < 3; i++) {			
+			String c = sc.nextLine();
+			sb.append(c+" ");
+		}
+		
+		sc.close();
+		
+		System.out.println(sb.reverse().toString().trim());
+	}
+
+}
