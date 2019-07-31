46333a57731d99390ffb1cb5d1bf1da16e7a28f2
rognyanov <radoslav.ognyanov@outlook.com>
2019-07-31 16:36:08 +0300

    Exercise / 4.Sum of Chars

---
+++ Data Types and Variables/Exercises/Sum of Chars/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int inputCount = Integer.parseInt(sc.nextLine());
+		int sum = 0;
+		
+		for (int i = 0; i < inputCount; i++) {
+			char newChar = sc.nextLine().toCharArray()[0];
+			sum+=newChar;
+		}
+		
+		System.out.printf("The sum equals: %d", sum);
+	}
+
+}
