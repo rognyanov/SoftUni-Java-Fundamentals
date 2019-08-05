c9155646788535ecf359ff7f20ca6305b43aa064
rognyanov <radoslav.ognyanov@outlook.com>
2019-08-05 11:57:13 +0300

    Exercise / 8.Magic Sum

---
+++ Arrays/Magic Sum/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Integer[] numbers = Stream.of(scanner.nextLine().split(" "))
+				.mapToInt(Integer::valueOf)
+				.boxed()
+				.toArray(Integer[]::new);
+		
+		Integer wantedSum = Integer.parseInt(scanner.nextLine());
+		
+		scanner.close();
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i+1; j < numbers.length; j++) {
+				if(numbers[i]+numbers[j] == wantedSum.intValue()) {
+					System.out.printf("%d %d%n", numbers[i], numbers[j]);
+				}
+			}
+		}
+	}
+
+}
